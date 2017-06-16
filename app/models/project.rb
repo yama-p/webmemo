522290f78e960b8cfc9d0213e3a407e7cd243cc7
yama-p <railgunninja1985@gmail.com>
2017-06-16 11:52:59 +0900

    [modify] stageし忘れ

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+  has_many :tasks
   validates :title,
             presence: { message: "Titleを入力してください" },
             length: { minimum: 3, message: "3文字以上入力してください" }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :projects
+  resources :projects do
+    resources :tasks, only: [:create, :destroy]
+  end
 
   root 'projects#index'
+
+  post '/projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
+
 end
