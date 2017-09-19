de2097f1b44857df93f618bdf1f0b6bec5083871
Kazuhito Maeyama <200454@MBPR0104.local>
2017-09-19 18:21:45 +0900

    試しにreact化

--- app/javascript/packs/devise/new.js
+++ app/javascript/packs/devise/new.js
@@ -1,51 +1,81 @@
 import React from 'react';
 
-class EMail extends React.Component {
-  constructor(){
+const EMail = ({email, onChange}) => {
+  return <div className="field">
+      <label className="title" for="user_email">Email</label>
+      <input autofocus="autofocus" id="user_email" type="email" name="user[email]" value={email} onChange={onChange} />;
+    </div>
+
+};
+
+const Password = ({onChange}) => {
+  return <div className="field">
+    <label className="title" for="user_password">パスワード</label>
+    <input autocomplete="off" type="password" name="user[password]" id="user_password" onChange={onChange} />
+  </div>
+};
+
+const RememberMe = ({checked, onChange}) => {
+  return <div className="field">
+      <p>
+        <input name="user[remember_me]" type="hidden" value="0" />
+        <input type="checkbox" value={checked} name="user[remember_me]" id="user_remember_me" onChange={onChange} />
+        remember me
+      </p>
+    </div>
+
+};
+
+class SettionsNew extends React.Component {
+
+  constructor() {
     super();
     this.state = {
-      email: ""
+      email: '',
+      password: '',
+      remember: '1',
     }
   }
 
+  onEmailChange = (e) => {
+    this.setState({
+      email: e.target.value
+    })
+  };
+
+  onPasswordChange = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  };
+
+  onRememberChange = (e) => {
+    this.setState({
+      remember: e.target.value
+    })
+  };
+
   render() {
-    return <input autofocus="autofocus" id="user_email" type="email" name="user[email]" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} />;
+    return (
+      <div className="devise" >
+        <h2>ログイン</h2>
+        <form className="new_user" id="new_user" action="/users/sign_in" accept-charset="UTF-8" method="post">
+          <input name="utf8" type="hidden" value="✓" />
+          <input type="hidden" name="authenticity_token" value="wL6W7pPUz69ezYUR+sDTGkl31hOzD6LSvBU/TizqX9c++kOhUVkzPL+BrntnN4whMnpOnkU/pvCCYWnessUnMw==" />
+
+          <Email email={this.state.email} onChange={this.onEmailChange} />
+          <Password onChange={this.onPasswordChange} />
+          <RememberMe checked={this.state.remember} onChange={this.onRememberChange} />
+
+          <div className="actions">
+            <input type="submit" name="commit" value="ログイン" data-disable-with="ログイン" />
+          </div>
+        </form>
+      </div>
+    )
   }
+
 }
-
-const SessionsNew = () => {
-  return (
-    <div className="devise" >
-      <h2>ログイン</h2>
-      <form className="new_user" id="new_user" action="/users/sign_in" accept-charset="UTF-8" method="post">
-        <input name="utf8" type="hidden" value="✓" />
-        <input type="hidden" name="authenticity_token" value="wL6W7pPUz69ezYUR+sDTGkl31hOzD6LSvBU/TizqX9c++kOhUVkzPL+BrntnN4whMnpOnkU/pvCCYWnessUnMw==" />
-
-        <div className="field">
-          <label className="title" for="user_email">Email</label>
-          <EMail/>
-        </div>
-
-        <div className="field">
-          <label className="title" for="user_パスワード">パスワード</label>
-          <input autocomplete="off" type="password" name="user[password]" id="user_password" />
-        </div>
-
-        <div className="field">
-          <p>
-            <input name="user[remember_me]" type="hidden" value="0" />
-            <input type="checkbox" value="1" name="user[remember_me]" id="user_remember_me" />
-            remember me
-          </p>
-        </div>
-
-        <div className="actions">
-          <input type="submit" name="commit" value="ログイン" data-disable-with="ログイン" />
-        </div>
-      </form>
-    </div>
-  )
-};
 
 export default SessionsNew;
 
