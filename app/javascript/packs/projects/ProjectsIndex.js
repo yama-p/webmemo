d8bb51a91aa6c6d81bdb7f19bb36d719dc8691fc
Kazuhito Maeyama <200454@MBPR0104.local>
2017-09-22 10:54:21 +0900

    projectsのindexをreact化準備

--- app/javascript/packs/projects/ProjectsIndex.js
+++ app/javascript/packs/projects/ProjectsIndex.js
@@ -1,21 +1,37 @@
+import React from 'react'
 
-<table border="1">
-  <tbody>
-    <tr>
-      <th>プロジェクト名</th>
-      <th>未完了</th>
-      <th>完了</th>
-      <th>全タスク数</th>
-      <th></th>
-      <th></th>
-    </tr>
-    <tr>
-      <td><a href="/yamap/1">PROJECT0</a></td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td><a href="/yamap/1/edit"> [編集]</a></td>
-      <td><a data-confirm="本当に削除しますがよろしいですか？" rel="nofollow" data-method="delete" href="/yamap/1">[削除]</a></td>
-    </tr>
-  </tbody>
-</table>
+
+class ProjectsIndex extends React {
+
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <table border="1">
+        <tbody>
+        <tr>
+          <th>プロジェクト名</th>
+          <th>未完了</th>
+          <th>完了</th>
+          <th>全タスク数</th>
+          <th></th>
+          <th></th>
+        </tr>
+        <tr>
+          <td><a href="/yamap/1">PROJECT0</a></td>
+          <td>0</td>
+          <td>0</td>
+          <td>0</td>
+          <td><a href="/yamap/1/edit"> [編集]</a></td>
+          <td><a data-confirm="本当に削除しますがよろしいですか？" rel="nofollow" data-method="delete" href="/yamap/1">[削除]</a></td>
+        </tr>
+        </tbody>
+      </table>
+    )
+  }
+}
